7d2ac566d54139a50f1b14335abbce7f669a1fa0
arctcogw <ogawa-r@arctec.co.jp>
2021-06-17 13:52:41 +0900

    change

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists(__DIR__.'myapp/public/../storage/framework/maintenance.php')) {
-    require __DIR__.'myapp/public/../storage/framework/maintenance.php';
+if (file_exists(__DIR__.'/myapp/storage/framework/maintenance.php')) {
+    require __DIR__.'/myapp/storage/framework/maintenance.php';
 }
 
 /*
@@ -31,7 +31,7 @@ if (file_exists(__DIR__.'myapp/public/../storage/framework/maintenance.php')) {
 |
 */
 
-require __DIR__.'myapp/public/../vendor/autoload.php';
+require __DIR__.'/myapp/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ require __DIR__.'myapp/public/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'myapp/public/../bootstrap/app.php';
+$app = require_once __DIR__.'/myapp/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
